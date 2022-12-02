cc5a68a22bf648007bb37f73e606cc0fe55d63ef
Ze Hua Chen <ze_hua_chen@brown.edu>
2022-12-02 17:12:38 -0500

    complete

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
 # Development
 
 ### Link to Deployed Website
-If you used the stencil code, this is `https://<your GitHub username>.github.io/<name of your repository>`
+If you used the stencil code, this is `https://risingbull888.github.io/development`
 
 ### Goal and Value of the Application
-
+My application 'The Garage' is website for wealthy car collectors to choose their showcase cars, with the ability to sort
+and filter by name, horsepower, and their colors. These functionality make it easier for them to choose what cars to 
+showcase during a swanky exotic car exhibition they attend every season. The user can filter through different colors and
+add the cars they want for the showcase and see the overall horsepower of their choices combined in order to adhere to the
+cumulative horsepower maximum set for each exhibition.
 ### Usability Principles Considered
 
+I made sure that minimum widths and heights were set for each card to make sure that they could still be viewed clearly
+at different screen sizes. The colors chosen are easy to read and will not have contrast errors. I used a hierarchy for
+text sizes and styles so the user can differentiate between the importance of the information. I put the side bar on the
+right so that it is easy to try out different filters and sorting to find the car they feel is suitable with ease. The
+side bar is always on the screen with the showcase cars displaying below it so that the user can always refer to the cars
+they have already added. I also chose to use accordion menus so that the user could hide the many options if they wanted
+to focus on a few characteristics, and also prepares for future implementation of more sorting options when there are more
+data about the cars (Year, Engine Type, Vehicle Type etc.)
+
 ### Organization of Components
+All text, cards and other elements are aligned for ease of reading. I used a grid to organize the main components on the
+page with the header of 'The Garage' as an exception. The CarCards are put into one column and the sidebar in the other.
+The CarCards themselves are organized using a simple grid and have text aligned left to adhere to reading conventions.
 
 ### How Data is Passed Down Through Components
 
-### How the User Triggers State Changes
+The app.js is the top-level, containing access to all the other elements of most importantly the CarCard and Sidebar. The
+The data of the cars are for the purposes of the demo stored in the App.js but should be imported through a .csv in the
+future when there are more cars. Within the app.js there are states for the showcase, totalHP, color, sortname, and sortHP.
+Showcase is the list of chosen showcase cars, total HP is total horsepower calculated in showcase cars, color is the color
+of the car, sortname is to indicate its sorting alphabetically, and sortHP is to indicate sorting by Horsepower ascending.
 
+Various functions then control these states according to the needs of the user. The data within app.js is passed into the
+CarCards for each different car, showing the image, names, horsepower, and color/s of the car. The sidebar then tells the
+app information on user clicks to change states and to indicate whether to filter or sort accordingly to the information
+of the cars.
+
+### How the User Triggers State Changes
+The user can trigger state changes through clicking the 'add to showcase' button which will change the showcase state.
+When they click add to showcase, an onClick triggers function calls that adds or removes the car from the list of showcase
+cars. They can also click the checkboxes on the sidecar to change the filter modes, different colors, and sorting options. By clicking
+any of these options, onChange triggers function calls that change the display of the CarCards, by removing, adding, or sorting
+them into the desired outcome.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import {Heading, ChakraProvider, Grid, GridItem, SimpleGrid, Box, Center} from '@chakra-ui/react';
+
+import CarCard from './components/CarCard';
+import Sidebar from './components/Sidebar';
+
 
 function App() {
+  const [color, setColor] = useState([]);
+  const [sortName, setSortName] = useState(false);
+  const [sortHP, setSortHP] = useState(false);
+  const [showcases, setShowcases] = useState([]);
+  const [totalHP, setTotalHP] = useState(0);
+
+
+  const data = [
+    { name: 'Bugatti Chiron', color1: 'Blue',color2: 'Black', total: 1500 },
+    { name: 'Aston Martin Zagato', color1: 'Red', color2: '', total: 510 },
+    { name: 'Lamborghini Aventador', color1 : 'Orange', color2: '', total: 759 },
+    { name: 'Ford GT MKII', color1: 'Purple', color2: 'Black', total: 700 },
+    { name: 'BMW i8', color1: 'Red', color2: 'Black', total: 369 },
+    { name: 'Lotus 3-Eleven', color1: 'Green', color2: 'Yellow', total: 430 },
+    { name: 'Jaguar F-Type', color1: 'Blue', color2: '', total: 575 },
+    { name: 'Bugatti Veyron', color1: 'Black', color2: 'Orange', total: 1200 },
+  ]
+
+  const addToShowcases = (car, add) => {
+    if (add) {
+      if (!showcases.includes(car)) {
+        setShowcases([...showcases, car[0]]);
+        setTotalHP(totalHP + car[1]);
+      }
+    } else {
+      const arr = [...showcases];
+      const idx = arr.indexOf(car[0]);
+      if (idx !== -1) {
+        arr.splice(idx, 1);
+        setShowcases(arr);
+        setTotalHP(totalHP - car[1]);
+      }
+    }
+  }
+
+  const isShowcase = (name) => {
+    return showcases.includes(name);
+  }
+
+  const selectFilter = (eventKey, add) => {
+    if (add) {
+      setColor([...color, eventKey]);
+    } else {
+      const array = [...color];
+      const idx = array.indexOf(eventKey);
+      if (idx !== -1) {
+        array.splice(idx, 1);
+        setColor(array);
+      }
+    }
+  }
+
+  const matchesFilter = (car) => {
+    if (color === []) {
+      return true;
+    } else {
+      for (let i = 0; i < color.length; i++) {
+        if (color[i] === 'Showcase') {
+          if (!showcases.includes(car.name)) {
+            return false;
+          }
+        } else if (car.color1 !== color[i] && car.color2 !== color[i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  const selectSort = (sort) => {
+    if (sort === 'name') {
+      setSortName(!sortName);
+    } else {
+      setSortHP(!sortHP);
+    }
+  }
+
+  let sorted = data.filter(matchesFilter).sort((x, y) => {
+    if (sortName && sortHP) {
+      const difference = x.name.localeCompare(y.name);
+
+      if (difference === 0) {
+        return x.total - y.total;
+      }
+      return difference;
+    } else if (sortName) {
+      return x.name.localeCompare(y.name);
+    } else if (sortHP) {
+      return x.total - y.total;
+    } else {
+      return 0;
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Center>
+        <Heading as='h1' size='3xl' py={10} color={'red'}>| The Garage |</Heading>
+      </Center>
+
+      <Grid templateColumns='repeat(5, 1fr)' templateRows='repeat(1, 1fr)' px={5}>
+        <GridItem colSpan={4}>
+          <Center>
+            <SimpleGrid
+              columns={[1, null, 2, null, 4]}
+              spacing={4}
+            >
+              {sorted.map((item, idx) =>
+                <Box key={idx}>
+                  <CarCard
+                    name={item.name}
+                    color1={item.color1}
+                    color2={item.color2}
+                    total={item.total}
+                    src={`${item.name.toLowerCase()}.png`}
+                    onClick={addToShowcases}
+                    isShowcase={isShowcase}
+                  />
+                </Box>)}
+            </SimpleGrid>
+          </Center>
+        </GridItem>
+
+        <GridItem colSpan={1}>
+          <Heading as='h2' size='md' py={3} px={3} color={'red'}>Sort & Filter</Heading>
+          <Sidebar
+              onSelect={selectFilter}
+              onSelectSort={selectSort}
+          />
+          <Box>
+            <Heading as='h3' size='md' py={3} px={3} color={'red'}>Showcase Cars</Heading>
+            {showcases.map((car, idx) =>
+                <Box key={idx} p={3} color='gray'>
+                  {car}
+                </Box>)}
+            <Heading as='h4' size='sm' py={3} px={3}>Total HP: {totalHP}</Heading>
+          </Box>
+        </GridItem>
+      </Grid>
+    </ChakraProvider>
   );
 }
 
